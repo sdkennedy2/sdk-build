56662b26d25c96a996566d1f56db956d16d8a03c
sdkennedy2 <scott@scottkennedy.ca>
2020-03-24 16:52:37 -0700

    Added package.json scripts

--- scaffold.bash
+++ scaffold.bash
@@ -2,8 +2,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 mkdir $1
 pushd $1
-mkdir -p src/node src/browser
-cp ${DIR}/.editorconfig
+cp ${DIR}/.editorconfig ./
 cp -R ${DIR}/static/ ./
 yarn install
 popd
