9563b794546eb224374cd91e70515215ed972396
sdkennedy2 <scott@scottkennedy.ca>
2020-03-24 17:18:03 -0700

    Added more debug

--- scaffold.bash
+++ scaffold.bash
@@ -2,12 +2,14 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 echo "DIR: $DIR"
 echo "ls ${DIR}/."
-ls ${DIR}/.
+ls -al ${DIR}/.
 echo "ls ${DIR}/.."
-ls ${DIR}/..
+ls -al ${DIR}/..
+echo "ls ${DIR}/../lib"
+ls -al ${DIR}/../lib
 mkdir $1
 pushd $1
 cp ${DIR}/.editorconfig ./
-cp -R ${DIR}/static/ ./
+cp -R ${DIR}/static ./
 yarn install
 popd
