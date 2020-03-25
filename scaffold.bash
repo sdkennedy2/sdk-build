adb9943b95580e3ead5865ff96d421dc5fe789e2
sdkennedy2 <scott@scottkennedy.ca>
2020-03-24 17:30:56 -0700

    Updated scaffolding DIR references to MODULE

--- scaffold.bash
+++ scaffold.bash
@@ -3,7 +3,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 MODULE="${DIR}/../lib/node_modules/sdk-build"
 mkdir $1
 pushd $1
-cp ${DIR}/.editorconfig ./
-cp -R ${DIR}/static ./
+cp ${MODULE}/.editorconfig ./
+cp -R ${MODULE}/static ./
 yarn install
 popd
