17ca02806528887843170f9b8c84084bd2c4922c
sdkennedy2 <scott@scottkennedy.ca>
2020-03-24 17:28:51 -0700

    Added mkdir back

--- scaffold.bash
+++ scaffold.bash
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 MODULE="${DIR}/../lib/node_modules/sdk-build"
+mkdir $1
 pushd $1
-cp ${MODULE}/.editorconfig ./
-cp -R ${MODULE}/static ./
+cp ${DIR}/.editorconfig ./
+cp -R ${DIR}/static ./
 yarn install
 popd
