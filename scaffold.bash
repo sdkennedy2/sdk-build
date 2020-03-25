4f0347712e26127ba01a12426e0416c7351fea92
sdkennedy2 <scott@scottkennedy.ca>
2020-03-24 17:54:05 -0700

    Fixed dev-node

--- scaffold.bash
+++ scaffold.bash
@@ -3,7 +3,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 MODULE="${DIR}/../lib/node_modules/sdk-build"
 mkdir $1
 pushd $1
-cp ${MODULE}/.editorconfig ./
 cp -R ${MODULE}/static/ ./
 yarn install
 popd
