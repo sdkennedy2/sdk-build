6a77235dedc66076c881775ba4987487ac674321
sdkennedy2 <scott@scottkennedy.ca>
2020-03-24 17:22:46 -0700

    Added debug

--- scaffold.bash
+++ scaffold.bash
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 echo "DIR: $DIR"
+echo "ls ."
+ls -al .
 echo "ls ${DIR}/."
 ls -al ${DIR}/.
 echo "ls ${DIR}/.."
 ls -al ${DIR}/..
 echo "ls ${DIR}/../lib"
 ls -al ${DIR}/../lib
+echo "ls ${DIR}/../lib/node_modules/sdk-build"
+ls -al ${DIR}/../lib/node_modules/sdk-build
 mkdir $1
 pushd $1
 cp ${DIR}/.editorconfig ./
