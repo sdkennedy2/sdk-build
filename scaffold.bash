ddb8a458c056188e76e1b00206ac2b358ced5c77
sdkennedy2 <scott@scottkennedy.ca>
2020-03-24 17:07:21 -0700

    Added debug

--- scaffold.bash
+++ scaffold.bash
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 echo "DIR: $DIR"
-echo "ls ."
-ls .
-echo "ls .."
-ls ..
+echo "ls ${DIR}/."
+ls ${DIR}/.
+echo "ls ${DIR}/.."
+ls ${DIR}/..
 mkdir $1
 pushd $1
 cp ${DIR}/.editorconfig ./
